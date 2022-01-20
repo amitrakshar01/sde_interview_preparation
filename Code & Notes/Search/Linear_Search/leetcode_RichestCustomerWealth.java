a157c6893e30ed1009e10f54b5905f5c3bffce11
Amitrakshar <sanyalamitrakshar@gmail.com>
2022-01-20 15:55:08 +0530

    Added Leetcode Problem2

---
+++ Code & Notes/Search/Linear_Search/leetcode_RichestCustomerWealth.java
@@ -0,0 +1,33 @@
+package Linear_Search;
+
+// https://leetcode.com/problems/richest-customer-wealth/
+
+/*  You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+    A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+*/ 
+
+/*  Input: accounts = [[1,5],[7,3],[3,5]]
+    Output: 10   */
+
+public class leetcode_RichestCustomerWealth{
+    public int maximumWealth(int[][] accounts) {
+        int total = customerWealth(accounts);
+        return total;
+    }
+    
+    static int customerWealth(int[][] arr){
+        int total = 0;
+        for(int i=0; i<arr.length; i++){
+            int sum = 0;
+            for(int j=0; j<arr[i].length; j++){
+                sum+=arr[i][j];
+            }
+            
+            if(sum>total){
+                total=sum;
+            }
+        }
+        return total;
+    }
+}
